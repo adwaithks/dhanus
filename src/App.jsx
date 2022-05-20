4332b169ca75cad8aeeb9c705d8eec7efd56e0f9
adwaithks <adwaith2507@gmail.com>
2022-05-20 14:32:58 +0530

    dhanus brochure

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import './App.css';
 import DhanusPosters from './components/DhanusPosters/DhanusPosters';
 import Footer from './components/Footer/Footer';
+import TopBanner from './components/TopBanner/TopBanner';
+import { TopBannerViewProvider } from './context/TopBannerViewContext';
 import Events from './pages/Events/Events';
 import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      {/*<DhanusPosters />*/}
-      <Footer />
-    </div>
+    <TopBannerViewProvider>
+      <div className="App">
+        <TopBanner />
+        <Home />
+        {/*<DhanusPosters />*/}
+        <Footer />
+      </div>
+    </TopBannerViewProvider>
   );
 }
 
