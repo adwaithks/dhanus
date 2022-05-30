fdfeb9b3723ee1adbec3ce3ec58cf3fab4444b1d
adwaithks <adwaith2507@gmail.com>
2022-05-30 23:33:31 +0530

    new posters + poster updates + poster links

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -18,6 +18,14 @@ function Events() {
               regAvail={true}
               regLink="https://docs.google.com/forms/d/e/1FAIpQLSeIfp5B25i5uPKiyWu1sD6UiPvJJhxDkkoYTg4aLF-henjr4w/viewform"
             />
+            <EventCard image="./gaming.jpg"
+              regAvail={true}
+              regLink="https://docs.google.com/forms/d/e/1FAIpQLSelISao4YdXIwmO_RknVFdrwUtvnstq8RzzjvtWSb2K8XxHEA/viewform?usp=sf_link"
+            />
+            <EventCard image="./engineeringdesign.jpg"
+              regAvail={true}
+              regLink="https://docs.google.com/forms/d/e/1FAIpQLScj3k_A8iQWQhwDEBmSt16XN4FlmyCamIOOwz-Dqqn12CbFeg/viewform"
+            />
             <EventCard image="./cktdebug.jpeg" 
                 regAvail={true} 
                 regLink="https://docs.google.com/forms/d/e/1FAIpQLSclhQUDF6v5hgrkedmrkh9EqPQiF71SFjLz6jORo8LNW_r2qg/viewform?usp=sf_link" 
@@ -26,18 +34,19 @@ function Events() {
               regAvail={true}
               regLink="https://docs.google.com/forms/d/e/1FAIpQLSc-FPBcTWvCMHFjkmVXmmasI_153qaYRbvd605oL5VRqiafhw/viewform"
             />
+            <EventCard image="./ejam.jpg"
+              regAvail={true}
+              regLink="https://docs.google.com/forms/d/e/1FAIpQLSeXSjTCGZkXyFihplBPmAdf7WAmVXqXzqHEEHOqUZ5zAYibQQ/viewform?usp=sf_link"
+            />
             <EventCard image="./hackathon.jpeg" />
             <EventCard image="./cpuassemble.jpg" />
             <EventCard image="./disandassemble.jpg" />
             <EventCard image="./oorja.jpeg" />
-            <EventCard image="./ejam.jpg" />
-            <EventCard image="./gaming.jpg" />
             <EventCard image="./paperpresentation.jpg" />
             <EventCard image="./uiuxdesign.jpeg" />
             <EventCard image="./iris.jpg" />
             <EventCard image="./workshop.jpg" />
             <EventCard image="./fastandfurious.jpg" />
-            <EventCard image="./engineeringdesign.jpg" />
         </div>
     </section>
   )
